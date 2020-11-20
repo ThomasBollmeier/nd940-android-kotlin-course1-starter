2adbbacd599f43d1075f2feaee13861b3efd0e95
Thomas Bollmeier <developer@thomas-bollmeier.de>
2020-11-20 21:57:15 +0100

    Fixed problem with missing action bar

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,29 +2,43 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Timber.plant(Timber.DebugTree())
-        
-        supportActionBar?.title = getString(R.string.app_name)
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
         setUpNavigation()
+
+        Timber.plant(Timber.DebugTree())
+    }
+
+    override fun onSupportNavigateUp() : Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 
     private fun setUpNavigation() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
-        //NavigationUI.setupActionBarWithNavController(this, navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph, null)
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
     }
+
+
+
 }
